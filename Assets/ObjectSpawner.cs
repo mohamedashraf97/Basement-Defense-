b1745af9945baa8652884ad81e3cc90720a5c03f
unknown <mohamedashraf892@gmail.com>
2020-03-27 23:48:27 +0200

    Initial commit

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    private PlacementIndicator placementIndicator ;
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Instantiate(objectToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+        }
+    }
+}
